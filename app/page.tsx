c09c75ca578a8c4830ea657f34a8565517e29233
Namka <namsrai.khatanbaatar@gmail.com>
2023-07-06 21:21:18 +0800

    css components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import Image from 'next/image'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello, Next.js!</h1>
-      <h2>suk</h2>
-    </main>
+    <div className="bg-black">
+      <Head>
+        <title>Namsrai Khatanbaatar</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen w-full bg-black shadow-md overflow-hidden">
+        <nav className="flex sm:justify-center space-x-4">
+          {[
+            ['Home', '/dashboard'],
+            ['Team', '/team'],
+            ['Projects', '/projects'],
+            ['Reports', '/reports'],
+          ].map(([title, url]) => (
+            <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+          ))}
+        </nav>
+      </div >
+      <div className="h-screen w-full  bg-accent1 shadow-md overflow-hidden">
+        <button className='btn-primary'>
+          Save changes
+        </button>
+      </div >
+    </div >
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        accent1: '#007148',
+        accent2: '#4CA346',
+        offWhite: '#F8ECE5',
+        caption: '#7D7873',
+        body: '#0D2329',
+        greyStrong: '#272625',
+        greyMedium: '#9D9995',
+        white: '#FFFFFF',
+        black: '#1E1B18',
+        red: '#F44336',
+      },
     },
   },
   plugins: [],
